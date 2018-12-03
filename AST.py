7c3b1b9ac019171fb7890327de8287de230a8cd8
KyleRosenberg <kyletherosenberg@icloud.com>
2018-12-02 17:07:25 -0700

    Combine AST and parser
    Added replace function to Expr
    Parser.parse now returns type(Expr)

--- AST.py
+++ AST.py
@@ -5,23 +5,34 @@
 class Expr:
     def __init__(self):
         pass
-    
+
     def getChild(self):
         pass
-    
+
     def __str__(self):
         pass
-    
+
     def __eq__(self, other):
         return self.__str__() == str(other)
+
+    def replace(self, exp1, exp2):
+        if self==exp1:
+            return exp2
+        else:
+            if isinstance(self, BinaryOp):
+                return type(self)(self.getFirstChild().replace(exp1, exp2), self.getSecondChild().replace(exp1, exp2))
+            elif isinstance(self, UnaryOp):
+                return type(self)(self.getChild().replace(exp1, exp2))
+            else:
+                return self
 
 class Const(Expr):
     def __init__(self, e):
         pass
-    
+
     def getValue(self):
         pass
-    
+
     def getChild(self):
         return None
 
@@ -36,23 +47,23 @@ class BinaryOp(Expr):
 
     def getFirstChild(self):
         return self.child1
-    
+
     def getSecondChild(self):
         return self.child2
-    
+
     def getChild(self):
         return [self.child1, self.child2]
 
     def setChild(self, e1, e2):
         self.child1 = e1
         self.child2 = e2
-    
+
     def setFirstChild(self, e1):
         self.child1 = e1
-    
+
     def setSecondChild(self, e2):
         self.child2 = e2
-    
+
 # This might not be necessary but whatever
 class UnaryOp(Expr):
     def __init__(self, e):
@@ -60,10 +71,10 @@ class UnaryOp(Expr):
             self.child = e
         else:
             raise ValueError("Invalid argument for Unary constructor")
-    
+
     def getChild(self):
         return self.child
-    
+
     def setChild(self, e):
         self.child = e
 
@@ -78,10 +89,10 @@ class Bool(Const):
             self.value = b
         else:
             raise ValueError("Invalid argumet for Bool constructor")
-    
+
     def __str__(self):
         return "Bool(" + str(self.value) + ")"
-    
+
     def __eq__(self, other):
         return self.__str__() == str(other)
 
@@ -91,10 +102,10 @@ class Var(Const):
             self.value = s
         else:
             raise ValueError("Invalid argument for Var constructor")
-    
+
     def __str__(self):
         return "Var(" + str(self.value) + ")"
-    
+
     def __eq__(self, other):
         return self.__str__() == str(other)
 
--- parser.py
+++ parser.py
@@ -1,38 +1,33 @@
 import re
 import time
 
+from AST import *
+
 class Parser:
 
     def __init__(self):
-        self.operators = ['~', '^', 'v', '->', '[=]'] #[Not, And, Or, Conditional, BiConditional]
+        self.operators = ['~', '^', 'v', 'x', '->', '[=]'] #[Not, And, Or, Xor, Conditional, BiConditional]
 
     def parse(self, str):
         str = str.replace(' ', '')
         if str.count('(')!=str.count(')'):
             raise Exception('Unequal number of opening and closing parentheses in expression')
-        try:
-            while str[0]=='(' and str[-1]==')':
-                str = str[1:-1]
-        except IndexError:
-            print(str)
+        while str[0]=='(' and str[-1]==')' and str.find(')') == len(str)-1 and str.rfind('(')==0:
+            str = str[1:-1]
         if str.count('(')==0:
             #Base case
             occ = [str.count(o) for o in self.operators]
             if sum(occ[1:]) > 1:
                 raise Exception('Only one operator is allowed per set of parentheses')
             if sum(occ[1:]) < 1:
-                ret = ''
-                for i in range(occ[0]):
-                    ret += 'Not('
-                val = str.replace(self.operators[0], '')
-                if val=='T':
-                    val = True
-                if val=='F':
-                    val = False
-                ret += 'Const(%s)' % val
-                for i in range(occ[0]):
-                    ret += ')'
-                return ret
+                if occ[0]==0:
+                    if str=='T':
+                        return Bool(True)
+                    if str=='F':
+                        return Bool(False)
+                    return Var(str)
+                else:
+                    return Not(self.parse(str[1:]))
             if sum(occ[1:])==1:
                 oper = ''
                 for i in range(1, len(occ)):
@@ -43,11 +38,23 @@ class Parser:
                 ind = str.index(oper)
                 p1 = self.parse(str[:ind])
                 p2 = self.parse(str[ind+len(oper):])
-                return 'Oper(%s, %s)' % (p1, p2)
+                if oper=='^':
+                    return And(p1, p2)
+                if oper=='v':
+                    return Or(p1, p2)
+                if oper=='x':
+                    return Xor(p1, p2)
+                if oper=='->':
+                    return Cond(p1, p2)
+                if oper=='[=]':
+                    return BiCond(p1, p2)
         else:
             close = str.find(')')
             open = str.rfind('(')
-            mid = self.parse(str[open+1:close])
-            temp = '%.20f' % time.time()
-            p = self.parse(str[:open] + temp + str[close+1:])
-            return p.replace('Const(%s)'%temp, mid)
+            if open < close:
+                mid = self.parse(str[open+1:close])
+                temp = '%.20f' % time.time()
+                p = self.parse(str[:open] + temp + str[close+1:])
+                return p.replace(Var(temp), mid)
+            else:
+                raise Exception('Input format not supported yet')
